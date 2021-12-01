33601ca80f866f68742d92d7164e78d071a7c63c
Sophia Moore <sms1390@nyu.edu>
2021-11-30 21:54:31 -0500

    initial commit

---
+++ api/recipe.controllers.js
@@ -0,0 +1,112 @@
+const mongoose = require("mongoose");
+const Recipe = mongoose.model("Recipe");
+
+exports.findAll = function (req, res) {
+  Recipe.find({}, function (err, results) {
+    return res.send(results);
+  });
+};
+
+exports.findById = (req, res) => {
+  const id = req.params.id;
+  Recipe.findOne({ _id: id }, (err, json) => {
+    if (err) return console.log(err);
+    return res.send(json);
+  });
+};
+
+exports.add = function (req, res) {
+  Recipe.create(req.body, function (err, recipe) {
+    if (err) return console.log(err);
+    return res.send(recipe);
+  });
+};
+
+exports.update = function (req, res) {
+  console.log(req.body);
+  const id = req.params.id;
+  Recipe.findByIdAndUpdate(id, req.body, { new: true }, (err, response) => {
+    if (err) return console.log(err);
+    res.send(response);
+  });
+};
+
+exports.delete = function (req, res) {
+  let id = req.params.id;
+  Recipe.deleteOne({ _id: id }, () => {
+    return res.sendStatus(202);
+  });
+};
+
+exports.upload = function (req, res) {
+  console.log(req.files);
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  let file = req.files.file;
+  file.mv(`./public/img/${req.body.filename}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ file: `public/img/${req.body.filename}` });
+    console.log(" res.json", res.json);
+  });
+};
+
+exports.import = function (req, res) {
+  Recipe.create(
+    {
+      title: "Lasagna",
+      description:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+      instructions:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+
+      image: "lasagna.png",
+      year: "2015",
+    },
+    {
+      title: "Pho-Chicken Noodle Soup",
+      description:
+        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+      instructions:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+
+      image: "pho.png",
+      year: "2016",
+    },
+
+    {
+      title: "Guacamole",
+      description:
+        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+      instructions:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+
+      image: "guacamole.png",
+      year: "2017",
+    },
+
+    {
+      title: "Hamburger",
+      description:
+        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+      instructions:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+
+      image: "hamburger.png",
+      year: "2018",
+    },
+    function (err) {
+      if (err) return console.log(err);
+      return res.sendStatus(201);
+    }
+  );
+};
+
+exports.killall = function (req, res) {
+  Recipe.deleteMany({}, (err) => {
+    if (err) return console.log(err);
+    return res.sendStatus(202);
+  });
+};
---
+++ api/recipe.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+const RecipeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  instructions: String,
+  image: String,
+  year: String,
+});
+
+const Recipe = mongoose.model("Recipe", RecipeSchema);
